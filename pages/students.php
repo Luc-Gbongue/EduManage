e951b5726ebc3e237fffea947f8456e093737f6d
Luc <zgbongue@gmail.com>
2026-06-28 04:42:46 +0000

    creation des fichiers edit et delete php

--- pages/students.php
+++ pages/students.php
@@ -106,8 +106,8 @@ $nombre_eleve = mysqli_num_rows($resultat_read);
                                 <td><?php echo $eleve['nom']; ?></td>
                                 <td><?php echo $eleve['prenom']; ?></td>
                                 <td><?php echo $eleve['classe']; ?></td>
-                                <td></td>
-                                <td></td>
+                                <td><a href="#">Modifier</a></td>
+                                <td><a href="#">Supprimer</a></td>
                                 </tr>
                                 <?php
                             }
